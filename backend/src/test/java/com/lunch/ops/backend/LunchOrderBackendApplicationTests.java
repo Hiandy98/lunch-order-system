584812b0c66f815596e6a54784b6e72c15d96fa4
Hiandy98 <mangoandy98@hotmail.com>
2026-09-01 22:45:03 +0800

    chore: initial repo structure with react-ts and spring-boot

---
+++ backend/src/test/java/com/lunch/ops/backend/LunchOrderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunch.ops.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchOrderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
