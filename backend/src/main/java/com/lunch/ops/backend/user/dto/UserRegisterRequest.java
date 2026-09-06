ff37242d88a463e7ca6f597c15526312ac61a944
Hiandy98 <mangoandy98@hotmail.com>
2026-09-06 15:03:20 +0800

    feat: implement user register request dto

---
+++ backend/src/main/java/com/lunch/ops/backend/user/dto/UserRegisterRequest.java
@@ -0,0 +1,14 @@
+package com.lunch.ops.backend.user.dto;
+
+public record UserRegisterRequest(
+        String id,  // 學號
+        String realName,
+        String nickName,
+        String classroom,
+        int number,  // 座號
+        String password
+) {
+    public UserRegisterCommand toCommand() {
+        return new UserRegisterCommand(id, realName, nickName, classroom, number, password);
+    }
+}
