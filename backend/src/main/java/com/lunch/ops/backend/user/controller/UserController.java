8baad1765992310c42b9f8dbe666a154ea302d18
Hiandy98 <mangoandy98@hotmail.com>
2026-09-10 22:46:20 +0800

    feat(user): implement get user information controller

--- backend/src/main/java/com/lunch/ops/backend/user/controller/UserController.java
+++ backend/src/main/java/com/lunch/ops/backend/user/controller/UserController.java
@@ -1,28 +1,35 @@
 package com.lunch.ops.backend.user.controller;
 
+import com.lunch.ops.backend.user.dto.UserInfoResponse;
 import com.lunch.ops.backend.user.dto.UserRegisterRequest;
 import com.lunch.ops.backend.user.dto.UserRegisterResponse;
 import com.lunch.ops.backend.user.service.UserRegisterService;
+import com.lunch.ops.backend.user.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
 
     private final UserRegisterService userRegisterService;
+    private final UserService userService;
 
-    public UserController(UserRegisterService userRegisterService) {
+    public UserController(UserRegisterService userRegisterService, UserService userService) {
         this.userRegisterService = userRegisterService;
+        this.userService = userService;
     }
 
     @PostMapping("/register")
     public ResponseEntity<UserRegisterResponse> register(@RequestBody UserRegisterRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(UserRegisterResponse.from(userRegisterService.execute(request.toCommand())));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserInfoResponse> me(@AuthenticationPrincipal String id) {
+        return ResponseEntity.ok(UserInfoResponse.from(userService.getUserById(id)));
     }
 }
