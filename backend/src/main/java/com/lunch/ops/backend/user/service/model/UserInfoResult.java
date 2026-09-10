8892c786054dcfe62d75289eab634a9da171e1d9
Hiandy98 <mangoandy98@hotmail.com>
2026-09-10 22:26:02 +0800

    feat(user): add UserInfoResult for UserService

---
+++ backend/src/main/java/com/lunch/ops/backend/user/service/model/UserInfoResult.java
@@ -0,0 +1,29 @@
+package com.lunch.ops.backend.user.service.model;
+
+
+import com.lunch.ops.backend.user.entity.Role;
+import com.lunch.ops.backend.user.entity.User;
+
+import java.time.LocalDateTime;
+
+public record UserInfoResult(
+        String id,
+        String realName,
+        String nickName,
+        String classroom,
+        int number,
+        Role role,
+        LocalDateTime updateAt
+) {
+    public static UserInfoResult form(User user) {
+        return new UserInfoResult(
+                user.getId(),
+                user.getRealName(),
+                user.getNickName(),
+                user.getClassroom(),
+                user.getNumber(),
+                user.getRole(),
+                user.getUpdatedAt()
+        );
+    }
+}
