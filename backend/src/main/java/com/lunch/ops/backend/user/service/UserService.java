664d7d85d313255bbda5744fade2a1d448341ec9
Hiandy98 <mangoandy98@hotmail.com>
2026-09-10 23:00:31 +0800

    feat(user): add updateUserInformation method

--- backend/src/main/java/com/lunch/ops/backend/user/service/UserService.java
+++ backend/src/main/java/com/lunch/ops/backend/user/service/UserService.java
@@ -1,7 +1,10 @@
 package com.lunch.ops.backend.user.service;
 
 import com.lunch.ops.backend.user.service.model.UserInfoResult;
+import com.lunch.ops.backend.user.service.model.UserUpdateCommand;
 
 public interface UserService {
     UserInfoResult getUserById(String id);
+
+    UserInfoResult updateUserInformation(UserUpdateCommand updateCommand);
 }
